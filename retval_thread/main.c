267b71f2b0dc3e739d5186a09e13ec065ae5692d
KOZAKI Tsuneaki <kozaki.tsuneaki@gmail.com>
2018-03-17 00:30:04 +0900

    Section 2-4 スレッドの消去

---
+++ retval_thread/main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <limits.h>
+
+int parse_repeat_count(int argc, char **argv);
+void* thread_func(void* arg);
+
+int main(int argc, char* argv[]) {
+
+    int n = parse_repeat_count(argc, argv);
+
+    pthread_t thread = 0;
+    if (pthread_create(&thread, NULL, thread_func, &n) != 0) {
+        fprintf(stderr, "Error: Failed to create new thread\n");
+        exit(1);
+    }
+
+    for (int i = 0; i < 5; ++i) {
+        printf("I'm main: %d\n", i);
+        sleep(1);
+    }
+
+    int* retval = NULL;
+    if (pthread_join(thread, (void**)&retval) != 0) {
+        fprintf(stderr, "Error: Failed to wait for the thread termination.\n");
+        exit(1);
+    }
+
+    printf("thread_func has been terminated with number %d\n", *retval);
+    free(retval);
+
+    printf("Bye...\n");
+
+    return 0;
+}
+
+int parse_repeat_count(int argc, char* argv[]) {
+
+    if (argc > 1) {
+        char* end = NULL;
+        long n = strtol(argv[1], &end, 10);
+        if (end != NULL && *end == '\0' && n <= INT_MAX) {
+            return (int)n;
+        }
+    }
+    return 1;
+}
+
+void* thread_func(void* arg) {
+
+    int n = *((int*)arg);
+
+    for (int i = 0; i < n; ++i) {
+        printf("I'm thread_func: %d\n", i);
+        sleep(1);
+    }
+
+    int* retval = malloc(sizeof(int));
+    *retval = n + 1000;
+    return retval;
+}
+
